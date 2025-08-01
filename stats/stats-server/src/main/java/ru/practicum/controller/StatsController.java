5b949a427c73825c00c719407753381551587cc2
Artem <antartemm@yandex.ru>
2025-08-01 08:41:37 +0300

    fix postman test

--- stats/stats-server/src/main/java/ru/practicum/controller/StatsController.java
+++ stats/stats-server/src/main/java/ru/practicum/controller/StatsController.java
@@ -1,8 +1,6 @@
 package ru.practicum.controller;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -16,8 +14,6 @@ import java.util.List;
 @RestController
 @RequestMapping
 @AllArgsConstructor
-@Getter
-@Setter
 public class StatsController {
     private final StatsService statsService;
 
@@ -28,12 +24,10 @@ public class StatsController {
     }
 
     @GetMapping("/stats")
-    public List<ViewStatsDto> getStats(@RequestParam(defaultValue = "0000-01-01 00:00:00")
-                                       @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime start,
-                                       @RequestParam(defaultValue = "4000-12-31 00:00:00")
-                                       @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime end,
+    public List<ViewStatsDto> getStats(@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime start,
+                                       @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime end,
                                        @RequestParam(required = false) List<String> uris,
-                                       @RequestParam(required = false,defaultValue = "false") Boolean unique) {
+                                       @RequestParam(defaultValue = "false") Boolean unique) {
         return statsService.getStats(start, end, uris, unique);
     }
 }
